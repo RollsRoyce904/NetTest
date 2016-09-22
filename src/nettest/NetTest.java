c73da060d332fe31e6c85e11a4896a174cd3ae8e
Royce Rhoden <slip4665@nefcom.net>
2016-09-22 15:42:35 -0400

    Network
    Networks Project test

---
+++ src/nettest/NetTest.java
@@ -0,0 +1,21 @@
+/*
+ * Author: Royce Rhoden
+ */
+package nettest;
+
+/**
+ *
+ * @author slip4
+ */
+public class NetTest
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        new NetMenu();
+    }
+    
+}
